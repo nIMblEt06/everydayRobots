4dedbfd9258bea4944622af3e5948659c4ea1e5e
Shwetabh <f20210923@pilani.bits-pilani.ac.in>
2022-01-28 01:40:05 +0530

    EOD commmit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-ht = document.getElementById("varHt");
-function scrollPlay(){
-    var width = window.pageYOffset;
-    ht.style.width = 80%;
+const cursorRounded = document.querySelector('.rounded');
+const moveCursor = (e) => {
+    const mouseY = e.clientY;
+    const mouseX = e.clientX;
+    cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
 }
+window.addEventListener('mousemove', moveCursor)
